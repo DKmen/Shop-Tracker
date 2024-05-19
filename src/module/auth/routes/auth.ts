1b020fd04c27dfe05305a8c966dba866baf2fc8f
DTech001 <dhrimil.mendapara@techholding.co>
2024-05-19 14:28:19 +0530

    feat: add route for auth module

--- src/module/auth/routes/auth.ts
+++ src/module/auth/routes/auth.ts
@@ -1,1 +1,10 @@
-// impliment the routes for the auth module
+import express from 'express'
+import { generateMegicLink, verifyMegicLink } from '../controller/auth'
+
+const router = express.Router()
+
+// user authentication
+router.post('/email/login', generateMegicLink)
+router.post('/token', verifyMegicLink)
+
+export default router
