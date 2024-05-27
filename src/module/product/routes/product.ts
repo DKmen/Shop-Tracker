ff6afecc3f03c36b6e3bebd148ba3a752bf8cc72
DTech001 <dhrimil.mendapara@techholding.co>
2024-05-27 15:49:41 +0530

    feat: add quantity logic into code

--- src/module/product/routes/product.ts
+++ src/module/product/routes/product.ts
@@ -5,6 +5,7 @@ import getProduct from '../controller/getProduct'
 import updateProduct from '../controller/updateProduct'
 import listProduct from '../controller/listProduct'
 import uploadCSVProduct from '../controller/uploadCSV'
+import addQuantity from '../controller/addQuantity'
 
 const router = express.Router()
 
@@ -14,5 +15,6 @@ router.get('/generate-csv', uploadCSVProduct)
 router.get('/:id', getProduct)
 router.patch('/:id', updateProduct)
 router.get('/', listProduct)
+router.post('/:id', addQuantity)
 
 export default router
